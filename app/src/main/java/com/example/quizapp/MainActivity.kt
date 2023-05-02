49f72963edd1151a29970478d36acb209c979e84
thedarkarchitect <romanreynolds240@gmail.com>
2023-05-02 16:28:16 +0300

    fourth commit

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.example.quizapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var button: Button
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState);
+        //remove the status bar the long way
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        this.window.setFlags(
+//            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+//            WindowManager.LayoutParams.FLAG_FULLSCREEN
+//        );
+//
+//        supportActionBar?.hide();
+        setContentView(R.layout.activity_main);
+        //short way to hide the status bar
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        supportActionBar?.hide()
+
+        button = findViewById(R.id.btn_Start)
+        var field : EditText = findViewById(R.id.et_name)
+
+        //target the inside of the text field using it's id
+        button.setOnClickListener{
+            if(field.text.toString().isEmpty()){
+                Toast.makeText(this, "please enter your name", Toast.LENGTH_SHORT).show()
+            }else{
+                
+            }
+        }
+
     }
 }
