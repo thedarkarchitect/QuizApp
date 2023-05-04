b6f7d8726acf198c1f913a3d743d8b2c2618f652
thedarkarchitect <romanreynolds240@gmail.com>
2023-05-04 21:22:18 +0300

    seventh commit

---
+++ app/src/main/java/com/example/quizapp/Constants.kt
@@ -0,0 +1,11 @@
+package com.example.quizapp
+
+object Constants{
+
+    fun getQuestions(): ArrayList<Question>{
+        val questionsList = ArrayList<Question>()
+
+        val que1 = Question(1, "What Country does this flag belong to?",
+        R.drawable.ic_udanda_flag, "United Kingdom", "Uganda", "Ukraine", "Uzbekistan")
+    }
+}
--- app/src/main/java/com/example/quizapp/Question.kt
+++ app/src/main/java/com/example/quizapp/Question.kt
@@ -1,5 +1,13 @@
 package com.example.quizapp
 
-data class Question {
-    val id : Int
-}
+data class Question(
+    val id: Int,
+    val question: String,
+    val image: Int,
+    val optionOne: String,
+    val optionTwo: String,
+    val optionThree: String,
+    val optionFour: String,
+    val correctAnswer: Int
+
+)
